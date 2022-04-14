73e08d11cc99486d2b7fc1427becf444bad4edac
Liam Jeanette <jeanet_l2@denison.edu>
2022-04-14 16:38:39 -0400

    almost there

--- sliding_window.h
+++ sliding_window.h
@@ -103,7 +103,7 @@ void send(char* hostname)
     setup_message[6] = 8 & 0xff; //len1
     setup_message[7] = (8 >> 8) & 0xff; //len2
 
-    int randInt = rand() % 10;
+    // int randInt = rand() % 10;
 
     int numbytes = sendto(sockfd, setup_message, 8, 0, ptr->ai_addr, ptr->ai_addrlen);
 
@@ -177,14 +177,20 @@ void send(char* hostname)
 
     // Listen for inputs on stdin and also listen for acks from other receiver
 
-    int last_ack_received = 0;
+    int last_seqnum_received = 0;
     int last_seqnum_sent = 0;
-    int new_seqnum_received = 0;
+    int next_seqnum_to_send = 1;
 
     char stored_message[MAXBUFLEN];
+    char message_from_stdin[MAXBUFLEN];
+
+    int message_from_stdin_len;
     
     memset(buffer, 0, MAXBUFLEN);
     memset(stored_message, 0, MAXBUFLEN);
+
+    struct sockaddr_storage sender_addr;	 // sender's address (may be IPv6)
+    socklen_t addr_len = sizeof sender_addr; // length of this address
 
     while (1)
     {
@@ -200,7 +206,7 @@ void send(char* hostname)
         {
             if (pfds[i].revents & POLLIN)
             {
-                // Message from other client
+                // Ack from receiver
                 if (pfds[i].fd == mysockfd)
                 {
                     // Receive ack from other receiver
@@ -213,25 +219,87 @@ void send(char* hostname)
                         break;
                     }
 
-                    new_seqnum_received = buffer[0] + (buffer[1] << 8) + (buffer[2] << 16) + (buffer[3] << 24);
+                    // Get sequence number of ack
+                    last_seqnum_received = buffer[0] + (buffer[1] << 8) + (buffer[2] << 16) + (buffer[3] << 24);
                     
-                    // If seqnum is same as seqnum just sent, we can send another message and delete old message and its timeout
+                    if (last_seqnum_sent == last_seqnum_received)
+                    {
+                        next_seqnum_to_send = (last_seqnum_sent + 1) % 2;
+                        memset(stored_message, 0, MAXBUFLEN);
+                    }
+                    else
+                    {
+                        cout << "seqnums don't match" << endl;
+                    }
 
-                    last_ack_received = new_seqnum_received;
-
-                    printf("message received from other client");
+                    printf("ack received from receiver");
                 }
                     
                 // Message from standard input
                 else if (pfds[i].fd == 0)
                 {
-                    // Choose new seqnum for message
+                    printf("message received from standard input\n");
+                    memset(buffer, 0, MAXBUFLEN);
+                    memset(message_from_stdin, 0, MAXBUFLEN);
+
+                    // get message from standard input
+
+                    fgets(message_from_stdin, MAXBUFLEN, stdin);
+
+                    message_from_stdin_len = strlen(message_from_stdin);
+                    
+                    if ((strlen(message_from_stdin) > 0) && (message_from_stdin[strlen (message_from_stdin) - 1] == '\n'))
+                        message_from_stdin[strlen (message_from_stdin) - 1] = '\0';
+                    
                     // Fill in byte array
-                    // Send buffer
-                    // If no ack received in certain time, send again
-                    printf("message received from stdin");
+
+                    buffer[0] = next_seqnum_to_send & 0xff;
+                    buffer[1] = (next_seqnum_to_send >> 8) & 0xff;
+                    buffer[2] = (next_seqnum_to_send >> 16) & 0xff;
+                    buffer[3] = (next_seqnum_to_send >> 24) & 0xff;
+                    buffer[4] = 0x00;
+                    buffer[5] = 0x00;
+                    buffer[6] = message_from_stdin_len && 0xff;
+                    buffer[7] = (message_from_stdin_len >> 8) & 0xff;
+
+                    for (int i = 0; i < message_from_stdin_len; i++)
+                    {
+                        buffer[i + 8] = message_from_stdin[i];
+                    } 
+ 
+                    numbytes = sendto(sockfd, buffer, message_from_stdin_len + 8, 0, ptr->ai_addr, ptr->ai_addrlen);
+
+                    last_seqnum_sent = next_seqnum_to_send;
+
+                    // get current time in order to keep track of timeout
+                    time_t now;
+                    time(&now);
+
+                    struct tm *local1 = localtime(&now);
+                    seconds1 = local1->tm_sec;
+                    
                 }
             }
+        }
+
+        // Get current time
+        time(&now);
+
+        struct tm *local1 = localtime(&now);
+        int seconds2 = local1->tm_sec;
+
+        int difference = (seconds2 - seconds1 < 0) ? seconds2 - seconds1 + 60 : seconds2 - seconds1; 
+
+        // See how long it has been since last message was sent
+        if (difference > TIMEOUT)
+        {
+            cout << "sender: timeout" << endl;
+            int numbytes = sendto(sockfd, setup_message, 8, 0, ptr->ai_addr, ptr->ai_addrlen);
+            
+            time(&now);
+
+            struct tm *local1 = localtime(&now);
+            seconds1 = local1->tm_sec;
         }
     }
 }
@@ -310,8 +378,10 @@ void receive(char* hostname)
     pfds[0].events = POLLIN; 
 
     char buffer[MAXBUFLEN];
+    unsigned char ack[MAXBUFLEN];
 
-    int last_received_seqnum;
+    int last_seqnum_sent;
+    int next_seqnum_to_send;
 
     struct sockaddr_storage sender_addr;	 // sender's address (may be IPv6)
 	socklen_t addr_len = sizeof sender_addr; // length of this address
@@ -354,7 +424,7 @@ void receive(char* hostname)
                 setup_ack[6] = buffer[6];
                 setup_ack[7] = buffer[7];
 
-                last_received_seqnum = setup_ack[0] + (setup_ack[1] << 8) + (setup_ack[2] << 16) + (setup_ack[3] << 24);
+                last_seqnum_sent = setup_ack[0] + (setup_ack[1] << 8) + (setup_ack[2] << 16) + (setup_ack[3] << 24);
 
                 int numbytes1 = sendto(sockfd, setup_ack, 8, 0, ptr->ai_addr, ptr->ai_addrlen);
 
@@ -369,7 +439,45 @@ void receive(char* hostname)
     // Receive messages and send acks until teardown message is received
     while(1)
     {
+        int poll_count = poll(pfds, 1, 5); 
 
+        if (poll_count == -1)
+        {
+            perror("poll");
+            exit(1);
+        }
+
+        if (pfds[0].revents & POLLIN)
+        {
+            // Ack from receiver
+            if (pfds[0].fd == mysockfd)
+            {
+                cout << "message received:";
+                // get sequence number then send back an ack
+                int numbytes = recvfrom(mysockfd, buffer, MAXBUFLEN - 1, 0, (struct sockaddr *)&sender_addr, &addr_len);
+
+                for (int i = 0; i < (buffer[6] + (buffer[7] << 8)); i++)
+                {
+                    cout << buffer[i + 8];
+                }
+
+                cout << "\n";
+
+                ack[0] = buffer[0];
+                ack[1] = buffer[1];
+                ack[2] = buffer[2];
+                ack[3] = buffer[3];
+                ack[4] = 0x01;
+                ack[5] = 0x00;
+                ack[6] = 8 & 0xff;
+                ack[7] = (8 >> 8) & 0xff;
+
+                int numbytes1 = sendto(sockfd, ack, 8, 0, ptr->ai_addr, ptr->ai_addrlen);
+
+                next_seqnum_to_send = (last_seqnum_sent + 1) % 2;
+
+                cout << "ack sent" << endl;
+            }
+        }
     }
-
 }
